0cb2d338966d5e219be21f5235af517b85f62176
Alexandre Lopes <alexandrefernadeslopes@gmail.com>
2024-05-15 15:11:11 +0100

    Triangular approximation algorithm

---
+++ src/Utils.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alexa on 14/05/2024.
+//
+
+#include "Utils.h"
---
+++ src/Utils.h
@@ -0,0 +1,16 @@
+//
+// Created by alexa on 14/05/2024.
+//
+
+#ifndef PROJETO_2_UTILS_H
+#define PROJETO_2_UTILS_H
+
+
+
+class Utils {
+
+};
+
+
+
+#endif //PROJETO_2_UTILS_H
