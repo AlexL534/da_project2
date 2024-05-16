fee3a58a6866becfbedb9cf5a8031f3fef2ab543
Alexandre Lopes <alexandrefernadeslopes@gmail.com>
2024-05-16 12:52:59 +0100

    fixed menu, some functionalities had stopped working + some results commented on main

--- src/main.cpp
+++ src/main.cpp
@@ -8,3 +8,38 @@ int main() {
     menu();
     return 0;
 }
+
+/*
+Vou guardar aqui alguns resultados
+Backtracking shipping:
+    Duration: 2.4e-02 seconds
+    Minimum cost: 86.7
+
+Backtracking stadiums:
+    Duration: 1.16e-02 seconds
+    Minimum cost: 341.0
+
+Backtracking tourism:
+    Duration: 7.50e-05 seconds
+    Minimum cost: 2600.0
+
+Triangular stadiums:
+    Duration: 2.80e-04 seconds
+    Minimum cost: 398.1
+
+Triangular tourism:
+    Duration: 8.30e-05 seconds
+    Minimum cost: 2600.0
+
+2-opt stadiums:
+    Duration: 3.07e-03 seconds
+    Minimum cost: 354.5
+
+2-opt tourism:
+    Duration: 1.24e-04 seconds
+    Minimum cost: 2600.0
+
+Triangular Real 1:
+    Duration: 6.67e+00 seconds
+    Minimum cost: 1121854.3
+*/
--- src/menu.h
+++ src/menu.h
@@ -68,6 +68,20 @@ void menu() {
                     }
                     break;
                 }
+                case 2: {
+                    std::vector<std::string> extraGraphs = {"edges_25", "edges_50", "edges_75", "edges_100", "edges_200", "edges_300", "edges_400", "edges_500", "edges_600", "edges_700", "edges_800", "edges_900"};
+                    std::cout << "Choose the graph:\n1. edges_25\n2. edges_50\n3. edges_75\n4. edges_100\n5. edges_200\n6. edges_300\n7. edges_400\n8. edges_500\n9. edges_600\n10. edges_700\n11. edges_800\n12. edges_900\n";
+                    std::cin >> graphChoice;
+                    graph = parseExtraFullyConnectedGraph("../Datasets/Extra_Fully_Connected_Graphs/Extra_Fully_Connected_Graphs/" + extraGraphs[graphChoice - 1] + ".csv", "../Datasets/Extra_Fully_Connected_Graphs/Extra_Fully_Connected_Graphs/nodes.csv");
+                    break;
+                }
+                case 3: {
+                    std::vector<std::string> realWorldGraphs = {"graph1", "graph2", "graph3"};
+                    std::cout << "Choose the graph:\n1. graph1\n2. graph2\n3. graph3\n";
+                    std::cin >> graphChoice;
+                    graph = parseRealWorldGraph("../Datasets/Real-world Graphs/Real-world Graphs/" + realWorldGraphs[graphChoice - 1] + "/edges.csv", "../Datasets/Real-world Graphs/Real-world Graphs/" + realWorldGraphs[graphChoice - 1] + "/nodes.csv");
+                    break;
+                }
                 default:
                     std::cout << "Invalid option. Please try again.\n";
                     std::cin.clear(); // Clear the error state
@@ -83,10 +97,10 @@ void menu() {
         }
 
         while (true) {
-            std::cout << "Choose the approach:\n1. Backtracking Algorithm\n2. Triangular Approximation Algorithm\n3. Go back\n";
+            std::cout << "Choose the approach:\n1. Backtracking Algorithm\n2. Triangular Approximation Algorithm\n3. 2-opt\n4. Go back\n";
             std::cin >> approachChoice;
 
-            if (approachChoice == 3) {
+            if (approachChoice == 4) {
                 goBackSelected = true;
                 break;
             }
@@ -111,6 +125,22 @@ void menu() {
 
                     auto start = std::chrono::high_resolution_clock::now();
                     double minCost = TSPTriangularApproximation(graph);
+                    auto end = std::chrono::high_resolution_clock::now();
+                    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+                    std::cout << "Duration: " << std::scientific << std::setprecision(2) << static_cast<double>(duration.count()) * 1e-6 << " seconds" << std::endl;
+                    std::cout << "Minimum cost: " << std::fixed << std::setprecision(1) << minCost << std::endl;
+                    return;
+                }
+                case 3:
+                {
+                    if (graphType == 1 && connectionType == 1) {
+                        std::cout << "This algorithm does not work with this graph (not fully connected and no coordinates given)\n\n";
+                        continue;
+                    }
+
+                    auto start = std::chrono::high_resolution_clock::now();
+                    double minCost = TSP2Opt(graph);
                     auto end = std::chrono::high_resolution_clock::now();
                     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 
